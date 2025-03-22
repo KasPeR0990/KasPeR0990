e2016d58d58ff2cc389b573fcf2a82ebc3254bc5
Kasper Hallenius <kasperhallenius@icloud.com>
2025-03-22 13:31:29 +0100

    Update layout.tsx

--- personal-website/app/layout.tsx
+++ personal-website/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=GeistMono:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
